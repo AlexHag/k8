8f40916a3e0c64f4fe30ca02493fbd33d49ef862
AlexHag <alex.hagdahl@gmail.com>
2026-02-20 17:29:34 +0100

    Rename secret accessor service account

--- opentofu/gke/secret-manager.tf
+++ opentofu/gke/secret-manager.tf
@@ -1,8 +1,8 @@
 
 # Google Secret Manager Accessor Service Account for GKE
-resource "google_service_account" "gke_secrets_accessor" {
-  account_id   = "gke-secrets-accessor"
-  display_name = "GKE Secret Manager Accessor"
+resource "google_service_account" "external_secrets" {
+  account_id   = "external-secrets-sa"
+  display_name = "External Secrets Operator Service Account"
   project      = var.project_id
 }
 
@@ -10,7 +10,7 @@ resource "google_service_account" "gke_secrets_accessor" {
 resource "google_project_iam_member" "secret_accessor" {
   project = var.project_id
   role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:${google_service_account.gke_secrets_accessor.email}"
+  member  = "serviceAccount:${google_service_account.external_secrets.email}"
 }
 
 locals {
@@ -21,7 +21,7 @@ locals {
 
 resource "google_service_account_iam_member" "workload_identity_binding" {
   for_each           = toset(local.wi_bindings)
-  service_account_id = google_service_account.gke_secrets_accessor.name
+  service_account_id = google_service_account.external_secrets.name
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.project_id}.svc.id.goog[${each.value}]"
 }

2940fe29fd14556a097d9e8ef1197389acdd83c5
AlexHag <alex.hagdahl@gmail.com>
2026-02-23 10:51:14 +0100

    Artifact Registry terraform WIP

---
+++ opentofu/artifact-registry/artifact-registry.tf
@@ -0,0 +1,33 @@
+resource "google_artifact_registry_repository" "docker" {
+  location      = var.region
+  repository_id = "application-images"
+  description   = "Docker image repository for containers running in GKE"
+  format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "delete-old-images"
+    action = "DELETE"
+
+    condition {
+      older_than = "2592000s" # 30 days in seconds
+      tag_state  = "UNTAGGED"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-recent-tagged"
+    action = "KEEP"
+
+    most_recent_versions {
+      keep_count = 10
+    }
+  }
+
+  project = var.project_id
+}
+
+# resource "google_artifact_registry_repository_iam_member" "ci_writer" {
+#   repository = google_artifact_registry_repository.docker.name
+#   role       = "roles/artifactregistry.writer"
+#   member     = "serviceAccount:${var.ci_service_account_email}"
+# }

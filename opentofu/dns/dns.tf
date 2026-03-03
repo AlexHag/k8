b9d5a3c05ea2007b385eed34b16832f038a14410
AlexHag <alex.hagdahl@gmail.com>
2026-03-03 17:47:22 +0100

    openclaw dns

--- opentofu/dns/dns.tf
+++ opentofu/dns/dns.tf
@@ -32,3 +32,11 @@ resource "google_dns_record_set" "sample-todo-app" {
   managed_zone = google_dns_managed_zone.main.name
   rrdatas      = [google_compute_address.envoy_gateway_lb.address]
 }
+
+resource "google_dns_record_set" "openclaw" {
+  name         = "openclaw.${var.fully_qualified_domain_name}"
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.main.name
+  rrdatas      = [google_compute_address.envoy_gateway_lb.address]
+}
